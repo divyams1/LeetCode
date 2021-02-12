a8d419bacd85f13b728e3b88f5182a1325f2661a
divyams1 <divyamsat@gmail.com>
2021-02-12 11:14:21 -0500

    min heap insert and remove

--- heap.js
+++ heap.js
@@ -21,5 +21,38 @@ class MinHeap {
 
     remove(node) {
         let smallest = this.heap[1];
+        if ( this.heap.length > 2) {
+            this.heap[1] = this.heap[this.heap.length-1];
+            this.heap.splice( this.heap.length-1);
+
+            if ( this.heap.length === 3) {
+                if ( this.heap[1] > this.heap[2]) {
+                    [ this.heap[1], this.heap[2] ] = [ this.heap[2], this.heap[1]]
+                }
+                return smallest;
+            }
+            let current = 1;
+            let leftChild = current * 2; 
+            let rightChild = current * 2 + 1;
+
+            while ( this.heap[leftChild] && this.heap[rightChild] && this.heap[current] > this.heap[leftChild] || this.heap[current] > this.heap[rightChild] ) {
+                if ( this.heap[leftChild] < this.heap[rightChild] ) {
+                    [ this.heap[current], this.heap[leftChild]] = [this.heap[leftChild], this.heap[current]]
+                    current = leftChild;
+                } else {
+                    [this.heap[current], this.heap[rightChildIndex]] = [this.heap[rightChildIndex], this.heap[current]]
+                    current = rightChildIndex
+                }
+                leftChild= current * 2
+                rightChild = current * 2 + 1
+            }
+        } else if ( this.heap.length === 2) {
+            this.heap.splice(1,1)
+        } else {
+            return null;
+        }
+        return smallest;
+        
+
     }
 }
